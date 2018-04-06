883c78d1f42a1a4ea0008bfa88bfa0339f5844bf
EtherComICO <38133611+EtherComICO@users.noreply.github.com>
2018-04-06 16:10:28 +0300

    Update ECT_emission_parts.sol

---
+++ ECT_emission_parts.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+contract ECT_emission_parts {
+  address bountyProgrammPartAddress   = 0xa3e785ee490e1102fe32512973c96dc7dc876348;
+  address businessAdvisorsPartAddress = 0xdb53ce480bac3ce96fdb79fae1156f80b6925e05;
+  address mainTeamPartAddress         = 0x80f5918c286fE7Feb7A861Dd28d0c5eF09916589;
+  address reserveFundPartAddress      = 0x2dd779d9eca2042a8b0daefe430e2a5494643c29;
+  address tokenSalesPartAddress       = 0x9bcb0a9d99d815bb87ee3191b1399b1bcc46dc77;
+}
